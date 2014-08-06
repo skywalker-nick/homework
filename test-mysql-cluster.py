fd3de7d0231cf0df42496b94b8f7be0e39027d88
Li Ma <skywalker.nick@gmail.com>
2014-08-06 00:28:28 -0700

    add read/write separation for sqlalchemy

--- test-mysql-cluster.py
+++ test-mysql-cluster.py
@@ -8,12 +8,14 @@ import time
 
 
 ENGINES = {
-    'master': 'mysql+mysqlconnector://root:password@192.168.3.188/testtest',
-    'slave1': 'mysql+mysqlconnector://root:password@192.168.3.189/testtest',
+    'master1': 'mysql+mysqlconnector://root:password@192.168.3.188/testtest',
+    'master2': 'mysql+mysqlconnector://root:password@192.168.3.189/testtest',
+    'slave1': 'mysql+mysqlconnector://root:password@192.168.3.191/testtest',
     'slave2': 'mysql+mysqlconnector://root:password@192.168.3.191/testtest',
 }
 
 
+master_keys = []
 slave_keys = []
 engines = {}
 for key in ENGINES.keys():
@@ -25,18 +27,25 @@ for key in ENGINES.keys():
     if re.match(r"^slave", key):
         slave_keys.append(key)
 
+    if re.match(r"^master", key):
+        master_keys.append(key)
+
 
 class RoutingSession(Session):
-    def get_bind(self, mapper=None, clause=None ):
+    def get_bind(self, mapper=None, clause=None):
         if self._name:
             return engines[self._name]
         elif self._flushing:
-            return engines['master']
+            return engines[
+                random.choice(master_keys)
+            ]
         else:
             return engines[
                random.choice(slave_keys)
             ]
+
     _name = None
+
     def using_bind(self, name):
         s = RoutingSession()
         vars(s).update(vars(self))
@@ -44,12 +53,12 @@ class RoutingSession(Session):
         return s
 
 
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         class_=RoutingSession))
-
+Session = sessionmaker(autocommit=True,
+                       autoflush=False,
+                       expire_on_commit=False,
+                       class_=RoutingSession)
+db_session = Session()
 Base = declarative_base()
-Base.query = db_session.query_property()
 
 
 def init_db(s):
@@ -71,20 +80,24 @@ class Entries(Base):
     title = Column(Text)
     text = Column(Text)
 
+    def __repr__(self):
+        return "<Entry('%s','%s','%s')>" \
+               % (self.id, self.title, self.text)
+
 
 for (k, v) in engines.items():
     init_db(v)
 
 
-print '############################### insert ################################'
-db_session.add_all([
-    Entries(title='entry_a', text='hoge'),
-    Entries(title='entry_b', text='fuga'),
-    Entries(title='entry_c', text='piyo'),
-])
-db_session.commit()
+print '############################### master insertion ################################'
+with db_session.begin(subtransactions=True):
+    db_session.add_all([
+        Entries(title='entry_a', text='hoge'),
+        Entries(title='entry_b', text='fuga'),
+        Entries(title='entry_c', text='piyo'),
+    ])
 
-print '###################### select (slave1 or slave2) ######################'
+print '###################### select (slave) ######################'
 rs = db_session.query(Entries).all()
 print_rs(rs)
 rs = db_session.query(Entries).all()
@@ -92,18 +105,40 @@ print_rs(rs)
 rs = db_session.query(Entries).all()
 print_rs(rs)
 
-print '######################## fix master select ############################'
-rs = db_session().using_bind("master").query(Entries).first()
-print_rs(rs)
+print '######################## master insersion ############################'
+with db_session.begin(subtransactions=True):
+    db_session.add_all([Entries(title=random.randint(1, 100), text=random.randint(1, 100)) for i in xrange(10)])
+    rs = db_session.query(Entries).all()
+    print_rs(rs)
 
-db_session.add_all([Entries(title=random.randint(1, 100), text=random.randint(1, 100)) for i in xrange(10)])
-db_session.commit()
-
-print '######################## transaction test #############################'
+print '######################## transaction insertion #############################'
 with db_session.begin(subtransactions=True):
     entry = Entries(title='entry_d', text='nick')
     db_session.add(entry)
 
+print '######################## transaction mixed operations 1 #############################'
 with db_session.begin(subtransactions=True):
-    query = db_session.query(Entries).filter_by(title='entry_b').one()
+    query = db_session.query(Entries).filter_by(title='entry_b').first()
+    print query
     query.text = 'changed'
+    query = db_session.query(Entries).filter_by(title='entry_b').first()
+    print query
+
+print '######################## transaction mixed operations 2 #############################'
+with db_session.begin(subtransactions=True):
+    query = db_session.query(Entries).delete()
+    rs = db_session.query(Entries).count()
+    print 'count: %d' % rs
+
+print '######################## verify transaction #############################'
+with db_session.begin(subtransactions=True):
+    rs = db_session.query(Entries).count()
+    print 'count 1: %d' % rs
+
+with db_session.begin(subtransactions=True):
+    rs = db_session.query(Entries).count()
+    print 'count 2: %d' % rs
+
+with db_session.begin(subtransactions=True):
+    rs = db_session.query(Entries).count()
+    print 'count 3: %d' % rs
